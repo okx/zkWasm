70b939b952b4da08464ee7ae979b778ff276b92a
cwbhhjl <cwbhhjl@gmail.com>
2023-08-16 08:59:54 +0800

    new log output

--- src/foreign/log_helper/mod.rs
+++ src/foreign/log_helper/mod.rs
@@ -1,4 +1,5 @@
 use std::cell::RefCell;
+use std::collections::HashMap;
 use std::rc::Rc;
 
 use specs::external_host_call_table::ExternalHostCallSignature;
@@ -10,19 +11,37 @@ use zkwasm_host_circuits::host::ForeignInst::Log;
 struct Context;
 impl ForeignContext for Context {}
 
-struct OutputContext {
-    output: Rc<RefCell<Vec<u64>>>,
+pub struct OutputContext {
+    pub output: Rc<RefCell<HashMap<u64, Vec<u64>>>>,
+    pub current_key: u64,
 }
 impl ForeignContext for OutputContext {}
 
 impl OutputContext {
-    pub fn new(output: Rc<RefCell<Vec<u64>>>) -> Self {
-        OutputContext { output }
+    pub fn new(output: Rc<RefCell<HashMap<u64, Vec<u64>>>>) -> Self {
+        OutputContext { output, current_key: 0 }
+    }
+
+    pub fn switch_key(&mut self, k: u64) {
+        self.current_key = k;
     }
 
     pub fn push(&self, v: u64) {
         let mut output = self.output.borrow_mut();
-        output.push(v);
+        if !output.contains_key(&self.current_key) {
+            output.insert(self.current_key.clone(), vec![]);
+        }
+        let target = output.get_mut(&self.current_key).unwrap();
+        target.push(v);
+    }
+
+    pub fn pop(&self) -> u64 {
+        let mut output = self.output.borrow_mut();
+        if let Some(target) = output.get_mut(&self.current_key) {
+            return target.pop().unwrap_or_default();
+        }
+
+        0
     }
 }
 
@@ -56,13 +75,38 @@ pub fn register_log_output_foreign(env: &mut HostEnv) {
         .external_env
         .register_plugin("foreign_log_output", Box::new(OutputContext::new(outputs)));
 
-    let get_output = Rc::new(
+    let push_output = Rc::new(
         |context: &mut dyn ForeignContext, args: wasmi::RuntimeArgs| {
             let context = context.downcast_mut::<OutputContext>().unwrap();
             let value: u64 = args.nth(0);
             context.push(value);
 
-            log::debug!("get internal output: {}", value);
+            log::debug!("internal output: {}", value);
+
+            None
+        },
+    );
+
+    let pop_output = Rc::new(
+        |context: &mut dyn ForeignContext, _args: wasmi::RuntimeArgs| {
+            let context = context.downcast_mut::<OutputContext>().unwrap();
+
+            let ret = context.pop();
+
+            log::debug!("pop internal output: {}", ret);
+
+            Some(wasmi::RuntimeValue::I64(ret as i64))
+        },
+    );
+
+    let switch_output = Rc::new(
+        |context: &mut dyn ForeignContext, args: wasmi::RuntimeArgs| {
+            let context = context.downcast_mut::<OutputContext>().unwrap();
+
+            let value: u64 = args.nth(0);
+            context.switch_key(value);
+
+            log::debug!("switch internal output: {}", value);
 
             None
         },
@@ -72,7 +116,23 @@ pub fn register_log_output_foreign(env: &mut HostEnv) {
         "wasm_log_output",
         std::mem::variant_count::<zkwasm_host_circuits::host::ForeignInst>(),
         ExternalHostCallSignature::Argument,
-        foreign_output_plugin,
-        get_output,
+        foreign_output_plugin.clone(),
+        push_output,
+    );
+
+    env.external_env.register_function(
+        "wasm_log_output_pop",
+        std::mem::variant_count::<zkwasm_host_circuits::host::ForeignInst>(),
+        ExternalHostCallSignature::Return,
+        foreign_output_plugin.clone(),
+        pop_output,
+    );
+
+    env.external_env.register_function(
+        "wasm_log_output_switch",
+        std::mem::variant_count::<zkwasm_host_circuits::host::ForeignInst>(),
+        ExternalHostCallSignature::Argument,
+        foreign_output_plugin.clone(),
+        switch_output,
     );
 }
