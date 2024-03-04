886505a704b92fe486fa49d6a806bbc453c2ee4c
Simonmhy <149035031+Simonmhy@users.noreply.github.com>
2024-03-04 13:50:11 +0800

    read write lock

--- crates/zkwasm/src/circuits/config.rs
+++ crates/zkwasm/src/circuits/config.rs
@@ -1,26 +1,23 @@
 use std::env;
-use std::sync::Mutex;
+use std::sync::RwLock;
 
 pub const POW_TABLE_POWER_START: u64 = 128;
-
 pub const MIN_K: u32 = 18;
-const MAX_K: u32 = 25;
 
 lazy_static! {
-    static ref ZKWASM_K: Mutex<u32> =
-        Mutex::new(env::var("ZKWASM_K").map_or(MIN_K, |k| k.parse().unwrap()));
+    static ref ZKWASM_K: RwLock<u32> =
+        RwLock::new(env::var("ZKWASM_K").map_or(MIN_K, |k| k.parse().unwrap()));
 }
 
 pub fn set_zkwasm_k(k: u32) {
+    println!("set set_zkwasm_k called here");
     assert!(k >= MIN_K);
-    assert!(k <= MAX_K);
-
-    let mut zkwasm_k = (*ZKWASM_K).lock().unwrap();
+    let mut zkwasm_k = ZKWASM_K.write().unwrap();
     *zkwasm_k = k;
 }
 
 pub fn zkwasm_k() -> u32 {
-    *ZKWASM_K.lock().unwrap()
+    *ZKWASM_K.read().unwrap()
 }
 
 pub fn init_zkwasm_runtime(k: u32) {
