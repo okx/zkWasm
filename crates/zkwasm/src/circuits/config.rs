273b6000b261dcfbb93958ef3b578e2ba861ab26
Simonmhy <149035031+Simonmhy@users.noreply.github.com>
2024-03-04 22:03:38 +0800

    atomic zkwasm k

--- crates/zkwasm/src/circuits/config.rs
+++ crates/zkwasm/src/circuits/config.rs
@@ -1,32 +1,28 @@
 use std::env;
-use std::sync::Mutex;
-
+use std::sync::atomic::{AtomicU32, Ordering};
+// use std::sync::RwLock;
 pub const POW_TABLE_POWER_START: u64 = 128;
-
 pub const MIN_K: u32 = 18;
 const MAX_K: u32 = 25;
-
 lazy_static! {
-    static ref ZKWASM_K: Mutex<u32> =
-        Mutex::new(env::var("ZKWASM_K").map_or(MIN_K, |k| k.parse().unwrap()));
+    // static ref ZKWASM_K: RwLock<u32> =
+    //     RwLock::new(env::var("ZKWASM_K").map_or(MIN_K, |k| k.parse().unwrap()));
+    static ref ZKWASM_K: AtomicU32 = AtomicU32::new(env::var("ZKWASM_K").map_or(MIN_K, |k| k.parse().unwrap()));
 }
-
 pub fn set_zkwasm_k(k: u32) {
     assert!(k >= MIN_K);
     assert!(k <= MAX_K);
-
-    let mut zkwasm_k = (*ZKWASM_K).lock().unwrap();
-    *zkwasm_k = k;
+    // let mut zkwasm_k = (*ZKWASM_K).write().unwrap();
+    // *zkwasm_k = k;
+    ZKWASM_K.store(k, Ordering::Relaxed);
 }
-
 pub fn zkwasm_k() -> u32 {
-    *ZKWASM_K.lock().unwrap()
+    // *ZKWASM_K.read().unwrap()
+    ZKWASM_K.load(Ordering::Relaxed)
 }
-
 pub fn init_zkwasm_runtime(k: u32) {
     set_zkwasm_k(k);
 }
-
 pub(crate) fn max_image_table_rows() -> u32 {
     8192
 }
