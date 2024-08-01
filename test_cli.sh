d9dee8de5a1b23a773bbc2c833a3b5d6468b4b04
chunfengSun <516108736@qq.com>
2024-08-01 16:46:42 +0800

    Update test_cli.sh

--- test_cli.sh
+++ test_cli.sh
@@ -11,10 +11,10 @@ test_default_cli() {
     cargo build --release $CUDA
     rm -rf params/*.data params/*.config output
     $CLI --params ./params wasm_output setup --wasm ./crates/zkwasm/wasm/wasm_output.wasm -k 23 --host standard
-#    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
-    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
-#    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
-    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
+    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
+#    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
+    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
+#    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
 #    $CLI --params ./params wasm_output verify --output ./output
 }
 
