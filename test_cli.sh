cf72a8b84672273e903526555ae03f84e4295894
chunfengSun <516108736@qq.com>
2024-08-01 11:44:21 +0800

    Update test_cli.sh

--- test_cli.sh
+++ test_cli.sh
@@ -10,10 +10,10 @@ CUDA="--features cuda"
 test_default_cli() {
     cargo build --release $CUDA
     rm -rf params/*.data params/*.config output
-    $CLI --params ./params wasm_output setup --wasm ./crates/zkwasm/wasm/wasm_output.wasm
-    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
-    $CLI --params ./params wasm_output verify --output ./output
+    $CLI --params ./params wasm_output setup --wasm ./crates/zkwasm/wasm/wasm_output.wasm -k 23 --host standard
+    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x040000000000000000010101:bytes-packed --output ./output
+#    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
+#    $CLI --params ./params wasm_output verify --output ./output
 }
 
 test_uniform_circuit_cli() {
