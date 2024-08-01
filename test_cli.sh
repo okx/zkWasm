d1b30e00575053fcd5d82d2ea7db172e30f8b0ea
chunfengSun <516108736@qq.com>
2024-08-01 15:27:37 +0800

    Update test_cli.sh

--- test_cli.sh
+++ test_cli.sh
@@ -11,8 +11,8 @@ test_default_cli() {
     cargo build --release $CUDA
     rm -rf params/*.data params/*.config output
     $CLI --params ./params wasm_output setup --wasm ./crates/zkwasm/wasm/wasm_output.wasm -k 23 --host standard
-    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x010000000000000003:bytes-packed --output ./output
-    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x010000000000000003:bytes-packed --output ./output
+    $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
+    $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
 #    $CLI --params ./params wasm_output verify --output ./output
 }
 
