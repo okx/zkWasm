55138c2e78fe0d16aac7e4dd485aef286fd70858
chunfengSun <516108736@qq.com>
2024-08-13 11:51:54 +0800

    Update test_cli.sh

--- test_cli.sh
+++ test_cli.sh
@@ -5,12 +5,12 @@ CLI=./target/release/zkwasm-cli
 set -e
 set -x
 
-CUDA="--features perf"
+CUDA="--features cuda"
 
 test_default_cli() {
     cargo build --release $CUDA
     rm -rf params/*.data params/*.config output
-    CUDA_VISIBLE_DEVICES=1 $CLI --params ./params wasm_output setup --wasm ./crates/zkwasm/wasm/wasm_output.wasm -k 23 --host standard
+    CUDA_VISIBLE_DEVICES=1 $CLI --params ./params wasm_output setup --wasm ./crates/zkwasm/wasm/wasm_output.wasm -k 22 --host standard
 #    CUDA_VISIBLE_DEVICES=1 $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
     CUDA_VISIBLE_DEVICES=1 $CLI --params ./params wasm_output dry-run --wasm crates/zkwasm/wasm/wasm_output.wasm --public 133:i64 --public 2:i64 --output ./output
 #    CUDA_VISIBLE_DEVICES=1 $CLI --params ./params wasm_output prove --wasm crates/zkwasm/wasm/wasm_output.wasm --private 0x00:bytes-packed --output ./output
